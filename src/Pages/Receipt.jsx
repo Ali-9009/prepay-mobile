3fdf2f4667c72c205f38d8531191def6e7318ac9
Ali-9009 <br6332160@gmail.com>
2026-07-01 16:31:50 +0500

    chg 2nd

--- src/Pages/Receipt.jsx
+++ src/Pages/Receipt.jsx
@@ -41,7 +41,7 @@ export default function Receipt() {
                 </ReceiptSection>
 
                 <ReceiptSection title="Product Details">
-                    <Row label="Carrier" value="AT & T" />
+                    <Row label="Carrier" value="Prepay" />
                     <Row label="Plan" value="Prepay $35" />
                     <Row label="Activation Date" value="03-03-2026" />
                     <Row label="SIM No." value="1111100000" />
